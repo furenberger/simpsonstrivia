831e0bfe4e26b071e2f1485073ffd8cb2694969a
furenberger <furenberger@gmail.com>
2018-05-20 14:24:19 -0500

    updates

--- index.js
+++ index.js
@@ -69,6 +69,8 @@ function populateRoundAnswers(
 }
 
 function isAnswerSlotValid(intent) {
+  console.log('answer: ', intent);
+  console.log('answervalue: ', intent.slots.Answer.value);
   const answerSlotFilled = intent
     && intent.slots
     && intent.slots.Answer
@@ -234,8 +236,8 @@ const languageString = {
   en: {
     translation: {
       QUESTIONS: questions.QUESTIONS_EN_US,
-      GAME_NAME: 'Reindeer Trivia',
-      HELP_MESSAGE: 'I will ask you %s multiple choice questions. Respond with the number of the answer. For example, say one, two, three, or four. To start a new game at any time, say, start game. ',
+      GAME_NAME: 'Simpsons Trivia',
+      HELP_MESSAGE: 'I will ask you %s multiple choice questions related to the Simpsons dude. Respond with the number of the answer. For example, say one, two, three, or four. To start a new game at any time, say, start game. ',
       REPEAT_QUESTION_MESSAGE: 'To repeat the last question, say, repeat. ',
       ASK_MESSAGE_START: 'Would you like to start playing?',
       HELP_REPROMPT: 'To give an answer to a question, respond with the number of the answer. ',
@@ -252,47 +254,16 @@ const languageString = {
       CORRECT_ANSWER_MESSAGE: 'The correct answer is %s: %s. ',
       ANSWER_IS_MESSAGE: 'That answer is ',
       TELL_QUESTION_MESSAGE: 'Question %s. %s ',
-      GAME_OVER_MESSAGE: 'You got %s out of %s questions correct. Thank you for playing!',
+      GAME_OVER_MESSAGE: 'You got %s out of %s questions correct. Thank you for playing Simpsons Trivia!',
       SCORE_IS_MESSAGE: 'Your score is %s. '
     },
   },
   'en-US': {
     translation: {
       QUESTIONS: questions.QUESTIONS_EN_US,
-      GAME_NAME: 'American Reindeer Trivia'
+      GAME_NAME: 'Simpsons Trivia'
     },
-  },
-  'en-GB': {
-    translation: {
-      QUESTIONS: questions.QUESTIONS_EN_GB,
-      GAME_NAME: 'British Reindeer Trivia'
-    },
-  },
-  de: {
-    translation: {
-      QUESTIONS: questions.QUESTIONS_DE_DE,
-      GAME_NAME: 'Wissenswertes über Rentiere in Deutsch',
-      HELP_MESSAGE: 'Ich stelle dir %s Multiple-Choice-Fragen. Antworte mit der Zahl, die zur richtigen Antwort gehört. Sage beispielsweise eins, zwei, drei oder vier. Du kannst jederzeit ein neues Spiel beginnen, sage einfach „Spiel starten“. ',
-      REPEAT_QUESTION_MESSAGE: 'Wenn die letzte Frage wiederholt werden soll, sage „Wiederholen“ ',
-      ASK_MESSAGE_START: 'Möchten Sie beginnen?',
-      HELP_REPROMPT: 'Wenn du eine Frage beantworten willst, antworte mit der Zahl, die zur richtigen Antwort gehört. ',
-      STOP_MESSAGE: 'Möchtest du weiterspielen?',
-      CANCEL_MESSAGE: 'OK, dann lass uns bald mal wieder spielen.',
-      NO_MESSAGE: 'OK, spielen wir ein andermal. Auf Wiedersehen!',
-      TRIVIA_UNHANDLED: 'Sagt eine Zahl beispielsweise zwischen 1 und %s',
-      HELP_UNHANDLED: 'Sage ja, um fortzufahren, oder nein, um das Spiel zu beenden.',
-      START_UNHANDLED: 'Du kannst jederzeit ein neues Spiel beginnen, sage einfach „Spiel starten“.',
-      NEW_GAME_MESSAGE: 'Willkommen bei %s. ',
-      WELCOME_MESSAGE: 'Ich stelle dir %s Fragen und du versuchst, so viele wie möglich richtig zu beantworten. Sage einfach die Zahl, die zur richtigen Antwort passt. Fangen wir an. ',
-      ANSWER_CORRECT_MESSAGE: 'Richtig. ',
-      ANSWER_WRONG_MESSAGE: 'Falsch. ',
-      CORRECT_ANSWER_MESSAGE: 'Die richtige Antwort ist %s: %s. ',
-      ANSWER_IS_MESSAGE: 'Diese Antwort ist ',
-      TELL_QUESTION_MESSAGE: 'Frage %s. %s ',
-      GAME_OVER_MESSAGE: 'Du hast %s von %s richtig beantwortet. Danke fürs Mitspielen!',
-      SCORE_IS_MESSAGE: 'Dein Ergebnis ist %s. '
-    },
-  },
+  }
 };
 
 
@@ -349,19 +320,19 @@ const UnhandledIntent = {
     const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
     if (Object.keys(sessionAttributes).length === 0) {
       const speechOutput = requestAttributes.t('START_UNHANDLED');
-      return handlerInput.attributesManager
+      return handlerInput.responseBuilder
         .speak(speechOutput)
         .reprompt(speechOutput)
         .getResponse();
     } else if (sessionAttributes.questions) {
       const speechOutput = requestAttributes.t('TRIVIA_UNHANDLED', ANSWER_COUNT.toString());
-      return handlerInput.attributesManager
+      return handlerInput.responseBuilder
         .speak(speechOutput)
         .reprompt(speechOutput)
         .getResponse();
     }
     const speechOutput = requestAttributes.t('HELP_UNHANDLED');
-    return handlerInput.attributesManager.speak(speechOutput).reprompt(speechOutput).getResponse();
+    return handlerInput.responseBuilder.speak(speechOutput).reprompt(speechOutput).getResponse();
   },
 };
 
@@ -384,6 +355,20 @@ const AnswerIntent = {
   },
   handle(handlerInput) {
     if (handlerInput.requestEnvelope.request.intent.name === 'AnswerIntent') {
+      return handleUserGuess(false, handlerInput);
+    }
+    return handleUserGuess(true, handlerInput);
+  },
+};
+
+const AnswerOnlyIntent = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+        && (handlerInput.requestEnvelope.request.intent.name === 'AnswerOnlyIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'DontKnowIntent');
+  },
+  handle(handlerInput) {
+    if (handlerInput.requestEnvelope.request.intent.name === 'AnswerOnlyIntent') {
       return handleUserGuess(false, handlerInput);
     }
     return handleUserGuess(true, handlerInput);
@@ -481,6 +466,7 @@ exports.handler = skillBuilder
     LaunchRequest,
     HelpIntent,
     AnswerIntent,
+    AnswerOnlyIntent,
     RepeatIntent,
     YesIntent,
     StopIntent,
